0371112e747559f0ed091bad87c82a5432edfd66
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2022-03-04 05:13:26 +0300

    Create Layer Model And Create platform Model

---
+++ Src/Redis.Models/Platform.cs
@@ -0,0 +1,15 @@
+﻿namespace Redis.Models;
+
+public class Platform
+{
+    public Platform()
+    {
+        Id = $"platform:{Guid.NewGuid()}";
+        Name = string.Empty;
+    }
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+
+}
